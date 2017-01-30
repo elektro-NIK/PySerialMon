46c0162a84eb698ac77d76ad352d13f8de07040f
Bohdan Kalinin <bogdan.kalinin@gmail.com>
2017-01-30 19:04:06 +0200

    Add create connection,  statusbar, update key.

--- main.py
+++ main.py
@@ -13,13 +13,64 @@ class MainWin(QMainWindow):
         super().__init__(parent)
         self.ui = mainwindow_ui.Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.comboBox_device.currentIndexChanged.connect(self.updatedescription)
-        self.coms = serial.tools.list_ports.comports()
-        for i in self.coms[::-1]:
-            self.ui.comboBox_device.addItem(i.device)
+        # Connects:
+        self.ui.pushButton_update.clicked.connect(self.updatedevices)
+        self.ui.pushButton_send.clicked.connect(self.senddata)
+        self.ui.comboBox_device.currentTextChanged.connect(self.updatedescription)
+        self.ui.comboBox_baud.currentTextChanged.connect(self.updatebaud)
+        # Devices:
+        self.coms = None
+        self.com = None
+        self.baud = None
+        self.con = None
+        self.updatedevices()
+        self.updatebaud(self.ui.comboBox_baud.currentText())
 
-    def updatedescription(self, a):
-        self.ui.label_description.setText(self.coms[a].description)
+    def updatedevices(self):
+        try:
+            self.con.close()
+        except AttributeError:
+            pass
+        self.ui.pushButton_send.setDisabled(True)
+        self.ui.comboBox_device.clear()
+        coms = serial.tools.list_ports.comports()
+        if len(coms) == 0:
+            self.ui.statusBar.showMessage('No serial adapter found!', msecs=3000)
+        self.coms = {i.device: i.description for i in coms[::-1]}
+        for i in self.coms.keys():
+            self.ui.comboBox_device.addItem(i)
+
+    def updatedescription(self, dev):
+        if dev:
+            self.ui.label_description.setText(self.coms[dev])
+            self.com = dev
+            if self.baud:
+                self.con = serial.Serial(self.com, self.baud, timeout=0.1)
+                self.ui.statusBar.showMessage('Connect to {0} with baud = {1}'.format(self.com,  self.baud), msecs=5000)
+                self.ui.pushButton_send.setEnabled(True)
+        else:
+            self.com = None
+
+    def updatebaud(self, baud):
+        try:
+            self.con.close()
+        except AttributeError:
+            pass
+        self.ui.pushButton_send.setDisabled(True)
+        try:
+            self.baud = int(baud)
+            self.ui.comboBox_baud.setStyleSheet(None)
+            if self.com:
+                self.con = serial.Serial(self.com, self.baud, timeout=0.1)
+                self.ui.statusBar.showMessage('Connect to {0} with baud = {1}'.format(self.com,  self.baud), msecs=5000)
+                self.ui.pushButton_send.setEnabled(True)
+        except ValueError:
+            self.baud = None
+            self.ui.comboBox_baud.setStyleSheet('background:#ff0000')
+            self.ui.statusBar.showMessage('Bad baud value!', msecs=3000)
+
+    def senddata(self):
+        print('1')
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
