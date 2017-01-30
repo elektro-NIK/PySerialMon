e98abe25d3a7b9eaea87c6af5c1bdb99483aa9ec
Bohdan Kalinin <bogdan.kalinin@gmail.com>
2017-01-30 16:07:02 +0200

    Add autodetect serial ports

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
+import serial.tools.list_ports
+import serial
 import mainwindow_ui
 from PyQt5.QtWidgets import *
 
@@ -11,7 +13,13 @@ class MainWin(QMainWindow):
         super().__init__(parent)
         self.ui = mainwindow_ui.Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.comboBox_device.currentIndexChanged.connect(self.updatedescription)
+        self.coms = serial.tools.list_ports.comports()
+        for i in self.coms[::-1]:
+            self.ui.comboBox_device.addItem(i.device)
 
+    def updatedescription(self, a):
+        self.ui.label_description.setText(self.coms[a].description)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
