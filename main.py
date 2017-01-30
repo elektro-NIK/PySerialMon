e82cb6a0900060bdad014d535db1416a4891d152
Bohdan Kalinin <bogdan.kalinin@gmail.com>
2017-01-30 23:47:36 +0200

    Add creation & close connection, write and read data, endline.

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import serial.tools.list_ports
 import serial
 import mainwindow_ui
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
 
 
 class MainWin(QMainWindow):
@@ -13,28 +14,33 @@ class MainWin(QMainWindow):
         super().__init__(parent)
         self.ui = mainwindow_ui.Ui_MainWindow()
         self.ui.setupUi(self)
+        # Lineending
+        self.lineend = ['', '\n', '\r', '\r\n']
         # Connects:
         self.ui.pushButton_update.clicked.connect(self.updatedevices)
-        self.ui.pushButton_send.clicked.connect(self.senddata)
+        self.ui.pushButton_send.clicked.connect(self.writedata)
         self.ui.comboBox_device.currentTextChanged.connect(self.updatedescription)
         self.ui.comboBox_baud.currentTextChanged.connect(self.updatebaud)
-        # Devices:
+        # Timer:
+        self.autoupdate = None
+        # Connection:
         self.coms = None
         self.com = None
         self.baud = None
         self.con = None
+        # Start
         self.updatedevices()
         self.updatebaud(self.ui.comboBox_baud.currentText())
 
     def updatedevices(self):
         try:
-            self.con.close()
+            self.closeconnection()
         except AttributeError:
             pass
         self.ui.pushButton_send.setDisabled(True)
         self.ui.comboBox_device.clear()
         coms = serial.tools.list_ports.comports()
-        if len(coms) == 0:
+        if not len(coms):
             self.ui.statusBar.showMessage('No serial adapter found!', msecs=3000)
         self.coms = {i.device: i.description for i in coms[::-1]}
         for i in self.coms.keys():
@@ -44,33 +50,52 @@ class MainWin(QMainWindow):
         if dev:
             self.ui.label_description.setText(self.coms[dev])
             self.com = dev
-            if self.baud:
-                self.con = serial.Serial(self.com, self.baud, timeout=0.1)
-                self.ui.statusBar.showMessage('Connect to {0} with baud = {1}'.format(self.com,  self.baud), msecs=5000)
-                self.ui.pushButton_send.setEnabled(True)
+            self.con = self.createconnection() if self.baud else None
         else:
             self.com = None
 
     def updatebaud(self, baud):
         try:
-            self.con.close()
+            self.closeconnection()
         except AttributeError:
             pass
         self.ui.pushButton_send.setDisabled(True)
         try:
             self.baud = int(baud)
             self.ui.comboBox_baud.setStyleSheet(None)
-            if self.com:
-                self.con = serial.Serial(self.com, self.baud, timeout=0.1)
-                self.ui.statusBar.showMessage('Connect to {0} with baud = {1}'.format(self.com,  self.baud), msecs=5000)
-                self.ui.pushButton_send.setEnabled(True)
+            self.con = self.createconnection() if self.com else None
         except ValueError:
             self.baud = None
             self.ui.comboBox_baud.setStyleSheet('background:#ff0000')
             self.ui.statusBar.showMessage('Bad baud value!', msecs=3000)
 
-    def senddata(self):
-        print('1')
+    def createconnection(self):
+        self.ui.plainTextEdit_output.clear()
+        self.ui.statusBar.showMessage('Connect to {0} with baud = {1}'.format(self.com, self.baud), msecs=5000)
+        self.autoupdate = QTimer(self)
+        # noinspection PyUnresolvedReferences
+        self.autoupdate.timeout.connect(self.readdata)
+        self.autoupdate.start(10)
+        self.ui.pushButton_send.setEnabled(True)
+        return serial.Serial(self.com, self.baud, timeout=1)
+
+    def closeconnection(self):
+        self.con.close()
+        self.autoupdate.timeout.disconnect(self.readdata)
+        self.con = None
+
+    def writedata(self):
+        end = self.lineend[self.ui.comboBox_lineend.currentIndex()]
+        msg = self.ui.lineEdit_input.text() + end
+        self.ui.lineEdit_input.clear()
+        self.con.write(msg.encode())
+
+    def readdata(self):
+        if self.con.inWaiting():
+            self.ui.plainTextEdit_output.insertPlainText(self.con.readline().decode('utf-8'))
+            sb = self.ui.plainTextEdit_output.verticalScrollBar()
+            if self.ui.checkBox_autoscroll.isChecked():
+                sb.setValue(sb.maximum())
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
